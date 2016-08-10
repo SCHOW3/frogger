4203836c423fbfd0e911f5b0b24217bf95eb65bd
Alex <azsoh@uci.edu>
2016-08-09 17:53:49 -0700

    TODO code

---
+++ frogger.py
@@ -0,0 +1,12 @@
+##TODO Make class
+
+import pygame,os,sys, random
+
+from pygame.locals import *
+
+class Frog(pygame.sprite.Sprite):
+    def __init__(self):
+        ##TODO
+    def move(self):
+        ##TODO
+##TODO Make moving objects as classes
